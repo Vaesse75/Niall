0f9ae689c04856753dc567c446f7db3acc848c0b
Vaesse75 <vaesse@gmail.com>
2020-03-28 00:14:21 -0400

    Added birthday features.

--- bot.js
+++ bot.js
@@ -1,8 +1,13 @@
 /*
     Future Plans:
-        Fix workout.js
-        Implement birthday.js
-        Ping for quest warning (in reply to messages)
+		workout.js
+			Sync
+			Cron
+			Cleanup
+        quest.js functioning
+        bday.js
+			Sch to internal?
+			Passes (Sch and msg) not working?
 */
 
 // Set constants and variables
@@ -12,11 +17,24 @@ const auth = require('/home/plex/bots/authNiall.json');
 const fs = require('fs');
 const Ch = require('./ch.js');
 const Role = require('./role.js');
-const Bday = require('./bday.js');
+const Birthday = require('./bday.js');
+Sch=Birthday.Import();
 Rems=[];
 cronjobs=[];
-workoutinfo=[];
 module.exports=bot;
+
+//Announce functions
+announce=function(say) {
+	if (say) {
+		onconn.send(say);
+	};
+}
+test=function(say) {
+	if (say) {
+		console.log(say);
+	};
+}
+
 
 // Replace User reference with "friend" when no user referenced
 Mbr=function(mem,leadcap) {
@@ -28,9 +46,7 @@ Mbr=function(mem,leadcap) {
 
 // Initial setup
 Niall.on('ready', () => {
-	require('./workout.js');
-
-    // Define Ch and Role objects
+	// Define Ch and Role objects
 	Ch.set("inn","664197181846061080");
 	Ch.set("guide","664199483025915904");
 	Ch.set("quest","665311310581596160");
@@ -44,36 +60,49 @@ Niall.on('ready', () => {
     WorkoutRef = Role.ref("workout");
     
     // Wakeup message
-    var say=["Ahem.","*Adjusts himself in the corner he's hiding in.*","*Sits up.*"];
+    var say=["Ahem.","*Adjusts himself in the corner he's hiding in.*","*Sits up.*","*Coughs quietly.*","*Hums under his breath.*"];
 	onconn.send(say[Math.floor(Math.random()*say.length)]);
+	
+	// Function calls
+	workout=require('./workout.js');
+	workout.Schedule(workout.Daily,test);
+
+	//Bday
 });
 
 // Reply to messages
 Niall.on('message', msg => {
 	// Respond to any capitalization
-    input=msg.content.toLowerCase();
+    var input=msg.content.toLowerCase();
+	var chk=undefined;
 	
     // Triggered responses
-	if (input.match(/^!ping/)) {
-    }
-    if (input.match(/^!help/)||msg.content.match(/^help.*niall.*/)) {
-		msg.channel.send(Mbr(msg.member,1)+", here's what I can do!\r\n\r\n!tip - I'll give you a random tip.\r\n!quest - Use this when you send invite for a new quest and I'll let everyone know when there's an hour left until the quest is set to start. (Not working yet.)\r\n!help - I'll display this message.");
+	if (input.match(/^!help/)||msg.content.match(/^help.*niall.*/)) {
+		msg.channel.send(Mbr(msg.member,1)+", here's what I can do!\n\n**!tip** - I'll give you a random tip.\n**!bday** - Tell me your birthday so we can celebrate together.\n**!quest** - Use this when you send the invite for a new quest and I'll let everyone know when there's an hour left until the quest is set to start. (Not working yet.)\n**!help** - I'll display this message.");
 	}
-
+	if (input.match(/^!bday/)) {
+		chk=undefined;
+		chk=Birthday.Add(msg,announce);
+		if (chk) {
+			Sch=chk;
+		}
+	}
+	// Having errors passing Sch as a whole and msg
+	chk=undefined;
+	chk=Birthday.Check(msg.author.id,Sch[msg.author.id],announce);
+	if (chk) {
+		Sch=chk;
+	}
+	
 	// Modularized responses
-	social=require('./social.js')(input); // Social responses
-	if (social) {
-		msg.channel.send(social);
-	};
-	tips=require('./tips.js')(input); // Tips
-	if (tips) {
+	announce(require('./social.js')(input)); // Social responses
+	announce(require('./quest.js')(input)); // Quest announcer
+	say=require('./tips.js')(input); // Tips
+	if (say) {
 		var embed = new Discord.RichEmbed()
-			.setDescription(tips);
+			.setColor(ffffff)
+			.setDescription(say);
 		msg.channel.send({ embed });
-	};
-	quest=require('./quest.js')(input); // Quest
-	if (quest) {
-		msg.channel.send(quest);
 	};
 });
 
