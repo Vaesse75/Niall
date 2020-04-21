efb4f628e68d8df58a4d27568beaa9d98e5ec142
caseyomah <caseyomah@gmail.com>
2020-04-21 11:49:51 -0400

    Added logging to quest to try to locate double announce.

--- quest.js
+++ quest.js
@@ -6,11 +6,15 @@ var time;
 Add=function(msg,say,role) {
 	// On activation:
 	say(role+", new quest just posted! Another reminder will be sent to all Questers in 23 hours.\n\nUse **!Quester** to toggle whether you want to be pinged by these reminders.",msg.channel);
+	
 	// Calculate now plus 23 hours.  Record to a temp file.
 	var date=new Date();
 	date.setHours(date.getHours() + 23);
+	//date.setMinutes(date.getMinutes() + 2); // Short announce time for testing.
 	temp.set("quest",Number(date));
 	quest=temp.get("quest");
+	
+	// Schedule the announce.
 	Schedule(say,msg.channel,role);
 }
 
@@ -19,15 +23,20 @@ Schedule=function(say,chan,role) {
 	if (quest) {
 		var now=new Date();
 		var when=new Date(Number(quest));
+		
 		time=setTimeout(()=>{Announce(say,chan,role)},when-now);
+		time="";
 	}
 }
 
 Announce=function(say,chan,role) {
+	console.log("Quest before announce: "+quest);
 	say(role+", new quest will start in an hour. Last chance to accept.\n\nUse **!Quester** to toggle whether you want to be pinged by these reminders.",chan);
+	
 	temp.del("quest");
+	quest=temp.get("quest");
+	console.log("Quest after announce: "+quest);
 }
 
 module.exports.Add=Add;
 module.exports.Schedule=Schedule;
-module.exports.Announce=Announce;
